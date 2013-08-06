2694114d1b644a55675dce6f1170895c7b2432e3
Ben Johnson <benbjohnson@yahoo.com>
2013-08-06 15:37:08 -0600

    Add fnv1a CLI.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	log.SetFlags(log.Lshortfile)
+
+	// Open file if passed in.
+	var file *os.File
+	var err error
+	if flag.NArg() > 0 {
+		file, err = os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Slurp the input.
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Hash the input and print the hash to stdout.
+	h := fnv.New64a()
+	h.Reset()
+	h.Write(b)
+	hashcode := h.Sum64()
+	fmt.Println(hashcode)
+}
